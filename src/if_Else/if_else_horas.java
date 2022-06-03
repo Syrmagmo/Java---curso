b93daf203b5db633dcb5b4093631e24f8df7b024
syrmagmo <lincolnrichard2@gmail.com>
2022-06-03 18:28:15 -0300

    If_else

---
+++ src/if_Else/if_else_horas.java
@@ -0,0 +1,23 @@
+package if_Else;
+
+import java.util.Scanner;
+
+public class if_else_horas {
+
+public static void main (String [] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int hora;
+		
+		System.out.println("Quantas horas?");
+		hora = sc.nextInt();
+		
+		if(hora <= 12) {
+			System.out.println("bom dia");
+		}
+		else {
+			System.out.println("boa tarde");
+		}
+	}
+
+}
