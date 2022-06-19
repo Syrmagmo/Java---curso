dbadfacb54ff72b1a3c24806dfe3653e72931845
syrmagmo <lincolnrichard2@gmail.com>
2022-06-19 00:14:52 -0300

    Create Escopo_e_inicializacao.java

---
+++ src/Extras/Escopo_e_inicializacao.java
@@ -0,0 +1,20 @@
+package Extras;
+
+public class Escopo_e_inicializacao {
+
+	public static void main(String[] args) {
+		
+		double price = 400.00; // price iniciado
+		double discount; // discont não iniciado 
+		
+		if(price < 200.00) {
+			discount = price *0.1;
+		}
+		else {
+			discount = 0;
+		}
+		
+		System.out.println(discount);
+	}
+
+}
