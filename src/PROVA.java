9e7c7378ca27d944b2d242e9fe9a43512b960719
syrmagmo <lincolnrichard2@gmail.com>
2022-06-21 23:09:19 -0300

    Update PROVA.java

--- src/PROVA.java
+++ src/PROVA.java
@@ -7,8 +7,8 @@ public class PROVA {
 	public static void main(String[] args) {
 		//	int array[] = new int [100];
 		//	int array[] = new int [1000];
-			int array[] = new int [10000];
-		//	int array[] = new int [500000];
+		//	int array[] = new int [10000];
+			int array[] = new int [500000];
 		//	int array[] = new int [1000000];
 		// int array[] = new int [1000000];
 
@@ -22,10 +22,13 @@ public class PROVA {
 		Instant instantInicial = Instant.now();
 
 		// selection(array);
-		// insertion(array); //rapido
-		 bubble(array);
+		// selectionDecrescente(array);
+		// insertion(array);
+		// insertionDecrescente(array);
+		// bubble(array);
 		// bubbleDecrescente(array);
 		// mergeSort(array, array, 0, array.length -1);
+		 mergeSortDecrescente(array, array, 0, array.length -1);
 		// quickSort(array, 0, array.length -1); // Um dos metodos mais rapidos de ordenação
 		// arraySort(array);
 
@@ -57,6 +60,22 @@ public class PROVA {
 			  }
 			}
 	
+	public static void selectionDecrescente(int array[]) {
+			for (int fixo = 0; fixo < array.length - 1; fixo++) {
+				  int menor = fixo;
+		  for (int i = menor + 1; i < array.length; i++) {
+				 if (array[i] > array[menor]) {
+					menor = i;
+				 }
+			  }
+			  if (menor != fixo) {
+				int t = array[fixo];
+				array[fixo] = array[menor];
+				array[menor] = t;
+			  }
+			}
+		  }
+			
 	public static void insertion(int array[]) {
 		 int j;
 		    int key;
@@ -73,6 +92,22 @@ public class PROVA {
 		    }
 		}
 		
+	public static void insertionDecrescente(int array[]) {
+		 int j;
+		    int key;
+		    int i;
+
+		    for (j = 1; j < array.length; j++)
+		    {
+		      key = array[j];
+		      for (i = j - 1; (i >= 0) && (array[i] < key); i--)
+		      {
+		    	  array[i + 1] = array[i];
+		       }
+		      array[i + 1] = key;
+		    }
+		}
+
 	public static void bubble(int[] vetor) {
 
 		boolean controle;
@@ -110,6 +145,44 @@ public class PROVA {
 			}			
 		}
 	}
+
+	public static void mergeSortDecrescente(int[] vetor, int[] aux, int inicio, int fim) {
+		if(inicio<fim)
+		{
+			int meio = (inicio + fim) / 2;
+			mergeSort(vetor, aux, inicio, meio);
+			mergeSort(vetor, aux, meio+1, fim);
+			intercalarDecrescente(vetor, aux, inicio, meio, fim);
+		}
+	}
+		private static void intercalarDecrescente(int[] vetor, int[] aux, int inicio, int meio, int fim) {
+			for(int i = inicio; i <= fim; i++)
+			{
+				aux[i] = vetor[i];
+			}
+			int esq = inicio;
+			int dir = meio+1;
+
+			for(int i=inicio; i<=fim; i++)
+			{
+				if(esq > meio)
+				{
+					vetor[i] = aux[dir++];
+				} 
+				else if (dir > fim)
+				{
+					vetor[i] = aux[esq++];
+				}
+				else if(aux[esq] < aux[dir])
+				{
+					vetor[i] = aux[esq++];
+				}
+				else
+				{
+					vetor[i] = aux[dir++];
+				}
+			}
+		}
 
 	public static void mergeSort(int[] vetor, int[] aux, int inicio, int fim) {
 		if(inicio<fim)
